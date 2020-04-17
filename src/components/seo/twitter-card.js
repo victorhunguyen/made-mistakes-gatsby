d5317a6ffb3fc962221228e179ec7a0ee0413ae5
Michael Rose <est.michael@gmail.com>
2020-04-17 16:03:30 -0400

    Fix react-helmet import

--- src/components/seo/twitter-card.js
+++ src/components/seo/twitter-card.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 const TwitterCard = ({ type, username, title, description, image }) => (
   <Helmet>
